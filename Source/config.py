6d8e1722a2c084d214892fa4c39c266c3da6c96a
alesanv <alejandra.sanvicente@gmail.com>
2021-09-13 21:52:27 -0700

    Adds configuration file

---
+++ Source/config.py
@@ -0,0 +1,15 @@
+#Weather configuration
+CITY = 'Portland'
+COUNTRYCODE = 'US'
+TEMP_UNIT = 'celsius'
+
+#Font
+FONT = '/home/pi/Projects/inkyphat/eInk_Display_Data/eInkDisplayInfo/Source/Resources/Roboto-Light.ttf'
+
+#Background image
+IMG_BACKGROUND = '/home/pi/Projects/inkyphat/eInk_Display_Data/eInkDisplayInfo/Source/Resources/bg1.png'
+
+#Stocks
+STOCK1 = 'GME'
+STOCK2 = 'ETSY'
+STOCK3 = 'TGT'
