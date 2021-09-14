aabdd69c4312af2329fba815527392b120e57ec3
alejandra.sanvicente <alejandra.sanvicente@gmail.com>
2021-09-13 22:03:46 -0700

    updates config file with general information

--- Source/config.py
+++ Source/config.py
@@ -1,15 +1,16 @@
-#Weather configuration
-CITY = 'Portland'
-COUNTRYCODE = 'US'
-TEMP_UNIT = 'celsius'
 
 #Font
-FONT = '/home/pi/Projects/inkyphat/eInk_Display_Data/eInkDisplayInfo/Source/Resources/Roboto-Light.ttf'
+FONT = # <path to font> Example: '/Resources/Roboto-Light.ttf'
 
 #Background image
-IMG_BACKGROUND = '/home/pi/Projects/inkyphat/eInk_Display_Data/eInkDisplayInfo/Source/Resources/bg1.png'
+IMG_BACKGROUND = #<Path to background image> Example: '/Resources/bg1.png'
+
+#Weather configuration
+CITY = # <City name> Example: 'Seattle'
+COUNTRYCODE = # <Country code> Example: 'US'
+TEMP_UNIT = # <celsius or farenheit> Example: 'celsius'
 
 #Stocks
-STOCK1 = 'GME'
-STOCK2 = 'ETSY'
-STOCK3 = 'TGT'
+STOCK1 = #Stock symbol #1 Example:'ETSY'
+STOCK2 = #Stock symbol #2 Example: 'TSLA'
+STOCK3 = #Stock symbol #3 Example: 'GME'
